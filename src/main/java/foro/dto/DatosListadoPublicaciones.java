a881e32f6f7b2b338901b72c8016894493bc0424
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-02 22:15:31 -0500

    Crea un DTO para dar formato a las respuestas a peticiones del listado de publicaciones

---
+++ src/main/java/foro/dto/DatosListadoPublicaciones.java
@@ -0,0 +1,13 @@
+package foro.dto;
+
+import java.time.LocalDateTime;
+
+import foro.modelo.EstadoPublicacion;
+import foro.modelo.Publicacion;
+
+public record DatosListadoPublicaciones(Long id, String titulo, String mensaje, LocalDateTime fechaCreacion, EstadoPublicacion estado) {
+
+	public DatosListadoPublicaciones(Publicacion publicacion) {
+		this(publicacion.getId(), publicacion.getTitulo(), publicacion.getMensaje(), publicacion.getFechaCreacion(), publicacion.getEstado());
+	}
+}
