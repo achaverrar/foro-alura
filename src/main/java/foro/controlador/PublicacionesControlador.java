f25d8f0707ac7880280a591d4ce22b4bf5263374
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-02 22:50:57 -0500

    Crea el método en el controlador para atender las peticiones del listado de publicaciones

--- src/main/java/foro/controlador/PublicacionesControlador.java
+++ src/main/java/foro/controlador/PublicacionesControlador.java
@@ -3,13 +3,19 @@ package foro.controlador;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import foro.dto.DatosListadoPublicaciones;
 import foro.dto.DatosNuevaPublicacion;
 import foro.dto.DatosRespuestaPublicacion;
 import foro.modelo.Publicacion;
@@ -21,6 +27,12 @@ import jakarta.validation.Valid;
 public class PublicacionesControlador {
 	@Autowired
 	private PublicacionRepositorio publicacionRepositorio;
+
+	@GetMapping
+	public Page<DatosListadoPublicaciones> listarPublicaciones(
+			@PageableDefault(size = 1, sort = {"fechaCreacion"}, direction = Direction.DESC) Pageable paginacion) {
+		return publicacionRepositorio.findAll(paginacion).map(DatosListadoPublicaciones::new);
+	}
 
 	@PostMapping
 	public ResponseEntity<DatosRespuestaPublicacion> crearPublicacion(@RequestBody @Valid DatosNuevaPublicacion datosPublicacion, UriComponentsBuilder uriComponentsBuilder) {
