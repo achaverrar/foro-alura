c2b88a6554fcc1b71ff5d99844e602cb12b683d6
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-02 21:22:42 -0500

    Crea un DTO para dar formato a las respuestas de peticiones relacionadas con la entidad Publicacion

---
+++ src/main/java/foro/dto/DatosRespuestaPublicacion.java
@@ -0,0 +1,13 @@
+package foro.dto;
+
+import java.time.LocalDateTime;
+
+import foro.modelo.EstadoPublicacion;
+import foro.modelo.Publicacion;
+
+public record DatosRespuestaPublicacion(Long id, String titulo, String mensaje, LocalDateTime fechaCreacion, EstadoPublicacion estado) {
+
+	public DatosRespuestaPublicacion(Publicacion publicacion) {
+		this(publicacion.getId(), publicacion.getTitulo(), publicacion.getMensaje(), publicacion.getFechaCreacion(), publicacion.getEstado());
+	}
+}
