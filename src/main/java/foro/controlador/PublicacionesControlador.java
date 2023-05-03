2b23feccfa7d91f6ca51adba722cbbc6c1c37b1c
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-02 21:23:53 -0500

    Estandariza las respuestas a las peticiones de creación de publicaciones

--- src/main/java/foro/controlador/PublicacionesControlador.java
+++ src/main/java/foro/controlador/PublicacionesControlador.java
@@ -1,12 +1,17 @@
 package foro.controlador;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import foro.dto.DatosNuevaPublicacion;
+import foro.dto.DatosRespuestaPublicacion;
 import foro.modelo.Publicacion;
 import foro.repositorio.PublicacionRepositorio;
 import jakarta.validation.Valid;
@@ -18,7 +23,17 @@ public class PublicacionesControlador {
 	private PublicacionRepositorio publicacionRepositorio;
 
 	@PostMapping
-	public void crearPublicacion(@RequestBody @Valid DatosNuevaPublicacion datosPublicacion) {
-		publicacionRepositorio.save(new Publicacion(datosPublicacion));
+	public ResponseEntity<DatosRespuestaPublicacion> crearPublicacion(@RequestBody @Valid DatosNuevaPublicacion datosPublicacion, UriComponentsBuilder uriComponentsBuilder) {
+		Publicacion publicacion = publicacionRepositorio.save(new Publicacion(datosPublicacion));
+
+		DatosRespuestaPublicacion datosRespuestaPublicacion = new DatosRespuestaPublicacion(
+				publicacion.getId(), 
+				publicacion.getTitulo(), 
+				publicacion.getMensaje(), 
+				publicacion.getFechaCreacion(), 
+				publicacion.getEstado());
+
+		URI url = uriComponentsBuilder.path("/publicaciones{id}").buildAndExpand(publicacion.getId()).toUri();
+		return ResponseEntity.created(url).body(datosRespuestaPublicacion);
 	}
 }
