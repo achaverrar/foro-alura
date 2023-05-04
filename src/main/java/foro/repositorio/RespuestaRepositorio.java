552898689c403a1bf9b4956bba8c2df6a2cbfb69
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-03 22:06:48 -0500

    Crea un método en el repositorio de la entidad Respuesta para obtener todas las respuestas a una publicación dado su id

--- src/main/java/foro/repositorio/RespuestaRepositorio.java
+++ src/main/java/foro/repositorio/RespuestaRepositorio.java
@@ -1,8 +1,12 @@
 package foro.repositorio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import foro.modelo.Respuesta;
 
 public interface RespuestaRepositorio extends JpaRepository<Respuesta, Long> {
+
+	Page<Respuesta> findAllByPublicacionId(Long publicacion_id, Pageable paginacion);
 }
