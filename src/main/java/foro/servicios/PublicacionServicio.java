2385f534c50aec7c74635e334b10d6736f3c0a48
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-03 20:57:59 -0500

    Crea una clase de servicio para la entidad Publicacion

---
+++ src/main/java/foro/servicios/PublicacionServicio.java
@@ -0,0 +1,53 @@
+package foro.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import foro.dto.publicaciones.DatosActualizacionPublicacion;
+import foro.dto.publicaciones.DatosListadoPublicaciones;
+import foro.dto.publicaciones.DatosNuevaPublicacion;
+import foro.dto.publicaciones.DatosRespuestaPublicacion;
+import foro.modelo.Publicacion;
+import foro.repositorio.PublicacionRepositorio;
+
+@Service
+public class PublicacionServicio {
+
+	@Autowired
+	private PublicacionRepositorio publicacionRepositorio;
+
+	public Page<DatosListadoPublicaciones> listarPublicaciones(Pageable paginacion) {
+		return publicacionRepositorio.findAll(paginacion).map(DatosListadoPublicaciones::new);
+	}
+
+	public DatosListadoPublicaciones encontrarPublicacionPorId(Long id) {
+		Publicacion publicacion = publicacionRepositorio.getReferenceById(id);
+		return new DatosListadoPublicaciones(publicacion);
+	}
+
+	public DatosRespuestaPublicacion crearPublicacion(DatosNuevaPublicacion datosPublicacion) {
+		Publicacion publicacion = publicacionRepositorio.save(new Publicacion(datosPublicacion));
+
+		DatosRespuestaPublicacion datosRespuestaPublicacion = new DatosRespuestaPublicacion(
+				publicacion.getId(), 
+				publicacion.getTitulo(), 
+				publicacion.getMensaje(), 
+				publicacion.getFechaCreacion(), 
+				publicacion.getEstado());
+
+		return datosRespuestaPublicacion;
+	}
+
+	public DatosRespuestaPublicacion editarPublicacion(DatosActualizacionPublicacion datosPublicacion) {
+        Publicacion publicacion = publicacionRepositorio.getReferenceById(datosPublicacion.id());
+        publicacion.editarPublicacion(datosPublicacion);
+        return new DatosRespuestaPublicacion(publicacion);
+    }
+
+	public void eliminarPublicacion(Long id) {
+		Publicacion publicacion = publicacionRepositorio.getReferenceById(id);
+		publicacionRepositorio.delete(publicacion);
+	}
+}
