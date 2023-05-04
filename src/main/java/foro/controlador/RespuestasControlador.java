a8e32bd210979e6caea333faedda28ef64035145
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-03 22:05:12 -0500

    Usa el servicio de la entidad Respuesta para refactorizar el código en su respectivo controlador

--- src/main/java/foro/controlador/RespuestasControlador.java
+++ src/main/java/foro/controlador/RespuestasControlador.java
@@ -12,28 +12,21 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import foro.dto.respuestas.DatosNuevaRespuesta;
 import foro.dto.respuestas.DatosRespuesta;
-import foro.modelo.Publicacion;
-import foro.modelo.Respuesta;
-import foro.repositorio.PublicacionRepositorio;
-import foro.repositorio.RespuestaRepositorio;
+import foro.servicios.RespuestaServicio;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/respuestas")
 public class RespuestasControlador {
 	@Autowired
-	private RespuestaRepositorio respuestaRepositorio;
-
-	@Autowired
-	private PublicacionRepositorio publicacionRepositorio;
+	private RespuestaServicio respuestaServicio;
 
 	@PostMapping
 	public ResponseEntity<DatosRespuesta> crearRespuesta(@RequestBody @Valid DatosNuevaRespuesta datosRespuesta, UriComponentsBuilder uriComponentsBuilder) {
-		Publicacion publicacion = publicacionRepositorio.getReferenceById(datosRespuesta.publicacion_id());
-		Respuesta respuesta = respuestaRepositorio.save(new Respuesta(datosRespuesta.mensaje(), publicacion));
-		DatosRespuesta datosRetornoRespuesta = new DatosRespuesta(respuesta);
+		DatosRespuesta respuesta = respuestaServicio.crearRespuesta(datosRespuesta);
 
-		URI url = uriComponentsBuilder.path("/respuestas{id}").buildAndExpand(publicacion.getId()).toUri();
-		return ResponseEntity.created(url).body(datosRetornoRespuesta);
+		URI url = uriComponentsBuilder.path("/respuestas{id}").buildAndExpand(respuesta.id()).toUri();
+		return ResponseEntity.created(url).body(respuesta);
 	}
+
 }
