d2321b08a83cf10c5bb3c42006416bcb771882cf
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-06 13:00:14 -0500

    Usa anotaciones en la clase Curso para mapearla a la base de datos

--- src/main/java/foro/modelo/Curso.java
+++ src/main/java/foro/modelo/Curso.java
@@ -1,62 +1,31 @@
 package foro.modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "Curso")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Table(name = "cursos", uniqueConstraints = { @UniqueConstraint(columnNames = { "nombre", "categoria" }) })
 public class Curso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(name = "nombre", nullable = false)
 	private String nombre;
+
+	@Column(name = "categoria", nullable = false)
 	private String categoria;
-
-	public Curso(String nombre, String categoria) {
-		this.nombre = nombre;
-		this.categoria = categoria;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Curso other = (Curso) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-
 }
