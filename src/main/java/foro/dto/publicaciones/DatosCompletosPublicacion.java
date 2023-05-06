2a9c1c68c6dbddff13c77848bc7bc862e1513e34
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-05 19:12:25 -0500

    Modifica el DTO de DatosCompletosPublicacion para que incluyan una lista de las respuestas a la publicación

--- src/main/java/foro/dto/publicaciones/DatosCompletosPublicacion.java
+++ src/main/java/foro/dto/publicaciones/DatosCompletosPublicacion.java
@@ -1,13 +1,27 @@
 package foro.dto.publicaciones;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import foro.dto.respuestas.DatosResumidosRespuesta;
 import foro.modelo.EstadoPublicacion;
 import foro.modelo.Publicacion;
 
-public record DatosCompletosPublicacion(Long id, String titulo, String mensaje, LocalDateTime fechaCreacion, EstadoPublicacion estado) {
+public record DatosCompletosPublicacion(
+		Long id, 
+		String titulo, 
+		String mensaje, 
+		LocalDateTime fechaCreacion, 
+		EstadoPublicacion estado, 
+		List<DatosResumidosRespuesta> respuestas) {
 
 	public DatosCompletosPublicacion(Publicacion publicacion) {
-		this(publicacion.getId(), publicacion.getTitulo(), publicacion.getMensaje(), publicacion.getFechaCreacion(), publicacion.getEstado());
+		this(
+				publicacion.getId(), 
+				publicacion.getTitulo(), 
+				publicacion.getMensaje(), 
+				publicacion.getFechaCreacion(), 
+				publicacion.getEstado(), 
+				publicacion.getRespuestas().stream().map(DatosResumidosRespuesta::new).toList());
 	}
 }
