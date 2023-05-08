f0e8aebdb52e5a3363fb6c41fd2a389c06308eb2
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-08 14:52:53 -0500

    Establece la lista de excepciones a manejar

--- src/main/java/foro/dto/excepciones/DatosErrorValidacion.java
+++ src/main/java/foro/dto/excepciones/DatosErrorValidacion.java
@@ -1,4 +1,4 @@
-package foro.dto;
+package foro.dto.excepciones;
 
 import org.springframework.validation.FieldError;
 
--- src/main/java/foro/excepciones/TratadoDeErrores.java
+++ src/main/java/foro/excepciones/TratadoDeErrores.java
@@ -5,24 +5,62 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import foro.dto.DatosErrorValidacion;
-import jakarta.el.MethodNotFoundException;
+import foro.dto.excepciones.DatosErrorValidacion;
 import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class TratadoDeErrores {
 
-	// Maneja los casos en que no se encuetra el recurso solicitado
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity tratarError404() {
-		return ResponseEntity.notFound().build();
-	}
-
-	// Maneja las peticiones inválidas
+	// Maneja las peticiones con información incompleta/nula
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity tratarError400(MethodArgumentNotValidException exception) {
-		var errores = exception.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+	public ResponseEntity<Object> tratarError400(MethodArgumentNotValidException excepcion) {
+		var errores = excepcion.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
 		return ResponseEntity.badRequest().body(errores);
 	}
 
+	// TODO: Maneja las peticiones cuyo cuerpo en JSON no se puede leer (porque está vacío o tiene un formato inválido)
+	// 400
+	// HttpMessageNotReadableException
+
+	// TODO: Información repetida, cuando debe ser única
+	// 400
+	// JdbcSQLIntegrityConstraintViolationException
+
+	// TODO: El id de la entidad original no existe
+	// 404
+	// EntityNotFoundException
+
+	// TODO: El id de la entidad padre no existe
+	// 400
+	// JdbcSQLIntegrityConstraintViolationException
+
+	// TODO: No pertenece a la entidad padre del id dado
+	// 400
+	// JdbcSQLIntegrityConstraintViolationException
+
+	// TODO: No hay una entidad padre para la lista que se pide
+	// 400
+	// Sin excepción nativa
+
+	// Maneja los casos en que no se encuetra el recurso solicitado
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<Object> tratarError404() {
+		return ResponseEntity.notFound().build();
+	}
+
+	// TODO: Usar un método inválido para un endpoint
+	// 405
+	// HttpRequestMethodNotSupportedException
+
+	// TODO: Formato requerido en la petición no es soportado
+	// 406
+	// HttpMediaTypeNotAcceptableException
+
+	// TODO: Formato proporcionado en la petición no es soportado
+	// 415
+	// HttpMediaTypeNotSupportedException
+
+	// TODO: Error del servidor
+	// 500
+	// Exception
 }
