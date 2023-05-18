e275e03c57be1a42d2878b4a0836b167352b74c4
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-18 07:08:55 -0500

    Usa Lombok para refactorizar el código en la clase Usuario y agrega las anotaciones para mapear la entidad a la base de datos

--- src/main/java/foro/modelo/Usuario.java
+++ src/main/java/foro/modelo/Usuario.java
@@ -1,65 +1,44 @@
 package foro.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import foro.excepciones.InfoExcepcionesPersonalizadas;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "Usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Table(name = "usuarios", uniqueConstraints = { @UniqueConstraint(columnNames = { "correo" }, 
+name = InfoExcepcionesPersonalizadas.USUARIO_CORREO_DUPLICADO) })
 public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private String nombre;
-	private String email;
+	private String correo;
 	private String contrasena;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getContrasena() {
-		return contrasena;
-	}
-
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name="usuarios_roles", 
+	joinColumns = @JoinColumn(name="usuario_id", referencedColumnName = "id"),
+    inverseJoinColumns=@JoinColumn(name="rol_id", referencedColumnName = "id")) 
+	private List<Rol> roles = new ArrayList<>();
 }
