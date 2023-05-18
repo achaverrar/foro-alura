b07eef68fd65d668edcd2b2a6c4730fa6c5d059c
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-18 08:12:09 -0500

    Crea la clase UsuarioSeguridad para manejar los detalles de usuario relacionados con la seguridad

---
+++ src/main/java/foro/seguridad/UsuarioSeguridad.java
@@ -0,0 +1,54 @@
+package foro.seguridad;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import foro.modelo.Usuario;
+
+public class UsuarioSeguridad implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	private final Usuario usuario;
+
+	public UsuarioSeguridad(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.usuario.getRoles();
+	}
+
+	@Override
+	public String getPassword() {
+		return this.usuario.getContrasena();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.usuario.getCorreo();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
