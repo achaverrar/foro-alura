9fdb9be976ec7593335d347e49a2e8c3676187ca
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-20 17:46:20 -0500

    Crea un método para registrar usuarios en UsuarioServicio

--- src/main/java/foro/servicios/UsuarioServicio.java
+++ src/main/java/foro/servicios/UsuarioServicio.java
@@ -1,14 +1,19 @@
 package foro.servicios;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import foro.dto.usuarios.DatosRegistroUsuario;
+import foro.modelo.Rol;
 import foro.modelo.Usuario;
+import foro.repositorio.RolRepositorio;
 import foro.repositorio.UsuarioRepositorio;
 import foro.seguridad.UsuarioSeguridad;
 
@@ -18,13 +23,35 @@ public class UsuarioServicio implements UserDetailsService {
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
 
+	@Autowired
+	private RolRepositorio rolRepositorio;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Override
 	public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException {
 		Optional<Usuario> usuario = usuarioRepositorio.findByCorreo(correo);
-		if(usuario.isPresent()) {
+		if (usuario.isPresent()) {
 			return new UsuarioSeguridad(usuario.get());
 		}
 		throw new UsernameNotFoundException("Credenciales inválidas");
 	}
 
+	public void registrarUsuario(DatosRegistroUsuario datosUsuario) {
+		String correo = datosUsuario.correo();
+		if (usuarioRepositorio.existsByCorreo(correo)) {
+			throw new RuntimeException("El correo electrónico ingresado ya se encuentra en uso");
+		}
+
+		Usuario usuario = new Usuario();
+		usuario.setNombre(datosUsuario.nombre());
+		usuario.setCorreo(correo);
+		usuario.setContrasena(passwordEncoder.encode(datosUsuario.contrasena()));
+
+		Rol rol = rolRepositorio.findByNombre("USUARIO").get();
+		usuario.setRoles(List.of(rol));
+
+		usuarioRepositorio.save(usuario);
+	}
 }
