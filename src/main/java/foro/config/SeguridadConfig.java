be50627ba7fc56a524b7c25111034758a388a024
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-20 11:12:57 -0500

    Configura un codificador de contraseñas y un administrador de autenticación en la clase de configuraciones

--- src/main/java/foro/config/SeguridadConfig.java
+++ src/main/java/foro/config/SeguridadConfig.java
@@ -2,8 +2,12 @@ package foro.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,15 @@ public class SeguridadConfig {
 				.permitAll()
 				.and()
 				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
