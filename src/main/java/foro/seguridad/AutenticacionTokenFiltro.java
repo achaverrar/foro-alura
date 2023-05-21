fbd8dcbb150d55c9900aa59c7a7bf6c3bac71ca6
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-21 08:29:15 -0500

    Crea un filtro de seguridad para verificar la validez de un token antes de admitir el acceso a recursos protegidos

---
+++ src/main/java/foro/seguridad/AutenticacionTokenFiltro.java
@@ -0,0 +1,54 @@
+package foro.seguridad;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import foro.repositorio.UsuarioRepositorio;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AutenticacionTokenFiltro extends OncePerRequestFilter {
+
+	@Autowired
+	private TokenServicio tokenServicio;
+
+	@Autowired
+	private UsuarioRepositorio usuarioRepositorio;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String jwToken = recuperarToken(request);
+
+		if (jwToken != null) {
+			String correo = tokenServicio.recuperarCorreoDeToken(jwToken);
+
+			UsuarioSeguridad usuario = new UsuarioSeguridad(usuarioRepositorio.findByCorreo(correo).get());
+
+			var autenticacion = new UsernamePasswordAuthenticationToken(
+					usuario,
+					null,
+					usuario.getAuthorities());
+
+			SecurityContextHolder.getContext().setAuthentication(autenticacion);
+
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	public String recuperarToken(HttpServletRequest peticion) {
+		String autorizacionHeader = peticion.getHeader("Authorization");
+		if (autorizacionHeader != null) {
+			return autorizacionHeader.replace("Bearer ", "");
+		}
+		return null;
+	}
+}
