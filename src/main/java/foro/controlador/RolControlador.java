a7b060e3b4244741b32df3ca3cd5a6d1ae582a5f
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-21 10:08:28 -0500

    Crea controlador para la entidad Rol e implementa la funcionalidad de creación de roles

---
+++ src/main/java/foro/controlador/RolControlador.java
@@ -0,0 +1,26 @@
+package foro.controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import foro.dto.roles.DatosRol;
+import foro.servicios.RolServicio;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/roles")
+public class RolControlador {
+
+	@Autowired
+	private RolServicio rolServicio;
+
+	@PostMapping
+	public ResponseEntity<Object> crearRol(@RequestBody @Valid DatosRol datosRol) {
+		rolServicio.crearRol(datosRol);
+		return ResponseEntity.ok().build();
+	}
+}
