38e5413a86a1b410194487d820415588f67845a7
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-21 10:51:52 -0500

    Recibe y maneja peticiones de asignación de nuevos roles a usuarios desde UsuarioControlador

--- src/main/java/foro/controlador/UsuarioControlador.java
+++ src/main/java/foro/controlador/UsuarioControlador.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,4 +46,14 @@ public class UsuarioControlador {
 		usuarioServicio.registrarUsuario(datosUsuario);
 		return ResponseEntity.ok().body("Registro exitoso");
 	}
+
+	@PostMapping("/{usuarioId}/roles/{rolId}")
+	@Transactional
+	public ResponseEntity<Object> asignarRolAUsuario(
+			@PathVariable Long usuarioId,
+			@PathVariable Long rolId) {
+		usuarioServicio.asignarRolAUsuario(usuarioId, rolId);
+		return ResponseEntity.ok().build();
+	}
+
 }
