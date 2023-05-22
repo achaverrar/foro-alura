d77398db8c6603381fe569ffe26a686d5ca1c13b
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-22 07:34:16 -0500

    Incluye al autor entre los datos que se envían al cliente tras crear una respuesta

--- src/main/java/foro/dto/respuestas/DatosCompletosRespuesta.java
+++ src/main/java/foro/dto/respuestas/DatosCompletosRespuesta.java
@@ -2,11 +2,24 @@ package foro.dto.respuestas;
 
 import java.time.LocalDateTime;
 
+import foro.dto.usuarios.DatosResumidosUsuario;
 import foro.modelo.Respuesta;
 
-public record DatosCompletosRespuesta(Long id, String mensaje, LocalDateTime fechaCreacion, Boolean solucion, Long publicacion_id) {
+public record DatosCompletosRespuesta(
+		Long id, 
+		String mensaje, 
+		LocalDateTime fechaCreacion, 
+		Boolean solucion, 
+		Long publicacion_id,
+		DatosResumidosUsuario autor) {
 
 	public DatosCompletosRespuesta(Respuesta respuesta) {
-		this(respuesta.getId(), respuesta.getMensaje(), respuesta.getFechaCreacion(), respuesta.getSolucion(), respuesta.getPublicacion().getId());		
+		this(
+				respuesta.getId(),
+				respuesta.getMensaje(),
+				respuesta.getFechaCreacion(),
+				respuesta.getSolucion(),
+				respuesta.getPublicacion().getId(),
+				new DatosResumidosUsuario(respuesta.getAutor()));		
 	}
 }
