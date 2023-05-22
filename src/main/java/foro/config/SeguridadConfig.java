137a21620eed9f27a9be10dcc95c98fc58c0587c
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-21 23:42:35 -0500

    Registra como bean a la clase de utilidades de seguridad

--- src/main/java/foro/config/SeguridadConfig.java
+++ src/main/java/foro/config/SeguridadConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,13 +15,18 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import foro.seguridad.AutenticacionTokenFiltro;
+import foro.seguridad.SeguridadUtilidades;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SeguridadConfig {
 
 	@Autowired
 	private AutenticacionTokenFiltro autenticacionTokenFiltro;
+
+	@Autowired
+	private SeguridadUtilidades seguridadUtilidades;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -49,4 +55,5 @@ public class SeguridadConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 }
