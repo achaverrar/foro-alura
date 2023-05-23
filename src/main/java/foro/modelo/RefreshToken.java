17b6b43789905f7926560281ece396c907092adb
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-23 17:54:04 -0500

    Crea una entidad para los refresh tokens y la mapea a la base de datos

---
+++ src/main/java/foro/modelo/RefreshToken.java
@@ -0,0 +1,44 @@
+package foro.modelo;
+
+import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "Refresh_Token")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class RefreshToken {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "token", nullable = false)
+	private String token;
+
+	@Column(name = "fecha_expiracion", nullable = false)
+	private LocalDateTime fechaExpiracion;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "usuario_id", referencedColumnName = "id")
+	private Usuario usuario;
+
+	public Date getFechaExpiracion() {
+		return Date.from(this.fechaExpiracion.atZone(ZoneId.systemDefault()).toInstant());
+	}
+}
