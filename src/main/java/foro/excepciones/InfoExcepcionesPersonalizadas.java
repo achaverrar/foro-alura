79843e339bd81285bf3ef4b87747df527fdc36bc
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-27 18:07:52 -0500

    Crea un mensaje personalizado para las excepciones por nombres de categorías duplicados

--- src/main/java/foro/excepciones/InfoExcepcionesPersonalizadas.java
+++ src/main/java/foro/excepciones/InfoExcepcionesPersonalizadas.java
@@ -9,13 +9,14 @@ public class InfoExcepcionesPersonalizadas {
 	public static final String RESPUESTA_MENSAJE_DUPLICADO = "RESPUESTA_MENSAJE_DUPLICADO";
 	public static final String USUARIO_CORREO_DUPLICADO = "USUARIO_CORREO_DUPLICADO";
 	public static final String ROL_NOMBRE_DUPLICADO = "ROL_NOMBRE_DUPLICADO";
+	public static final String CATEGORIA_NOMBRE_DUPLICADO = "CATEGORIA_NOMBRE_DUPLICADO";
 
 	public static final Map<String, String> MENSAJES = Map.of(
 			CURSO_NOMBRE_DUPLICADO, "El nombre del curso ya existe",
 			PUBLICACION_TITULO_MENSAJE_DUPLICADO, "Ya existe una publicación con este mensaje",
 			RESPUESTA_MENSAJE_DUPLICADO, "Esta respuesta ya existe en esta publicación",
 			USUARIO_CORREO_DUPLICADO, "Esta dirección de correo electrónico ya está en uso",
-			ROL_NOMBRE_DUPLICADO, "Este rol ya existe"
+			CATEGORIA_NOMBRE_DUPLICADO, "Ya existe una categoría con este nombre"
 			);
 
 }
