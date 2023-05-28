2fcf0bebcfd7abc33c46a5fe1cd3ec2c3612a0d0
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-28 16:18:24 -0500

    Crea un método en EtiquetaRepositorio para listar todas las etiquetas de un nivel dado

--- src/main/java/foro/repositorio/EtiquetaRepositorio.java
+++ src/main/java/foro/repositorio/EtiquetaRepositorio.java
@@ -1,8 +1,13 @@
 package foro.repositorio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import foro.modelo.Etiqueta;
+import foro.modelo.Nivel;
 
-public interface EtiquetaRepositorio extends JpaRepository<Etiqueta, Long>  {	
+public interface EtiquetaRepositorio extends JpaRepository<Etiqueta, Long>  {
+
+	Page<Etiqueta> findAllByNivel(Nivel nivel, Pageable paginacion);
 }
