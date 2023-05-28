679903107386603a213bb33b4318d4d2d3d80f45
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-28 17:36:08 -0500

    Atiende peticiones de obtener categorías por id desde CategoriaControlador

--- src/main/java/foro/controlador/CategoriaControlador.java
+++ src/main/java/foro/controlador/CategoriaControlador.java
@@ -8,12 +8,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import foro.dto.etiquetas.categorias.DatosCompletosCategoria;
 import foro.dto.etiquetas.categorias.DatosGuardarCategoria;
 import foro.dto.etiquetas.categorias.DatosResumidosCategoria;
 import foro.servicios.EtiquetaServicio;
@@ -31,6 +33,12 @@ public class CategoriaControlador {
 	public ResponseEntity<Page<DatosResumidosCategoria>> listarCategorias(@PageableDefault(size = 25) Pageable paginacion) {
 		var pagina = etiquetaServicio.listarCategorias(paginacion);
 		return ResponseEntity.ok(pagina);
+	}
+
+	@GetMapping("/{categoriaId}")
+	public ResponseEntity<DatosCompletosCategoria> encontrarCategoriaPorId(@PathVariable Long categoriaId) {
+		DatosCompletosCategoria categoria = etiquetaServicio.encontrarCategoriaPorId(categoriaId);
+		return ResponseEntity.ok(categoria);
 	}
 
 	@PostMapping
