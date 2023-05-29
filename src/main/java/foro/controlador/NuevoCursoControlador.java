80c9724af392cd049c94251dbc30e2341cea37c2
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-28 22:46:26 -0500

    Atiende solicitudes de creación de cursos desde el nuevo controlador de la entidad Curso

--- src/main/java/foro/controlador/NuevoCursoControlador.java
+++ src/main/java/foro/controlador/NuevoCursoControlador.java
@@ -1,10 +1,41 @@
 package foro.controlador;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import foro.dto.etiquetas.cursos.DatosCompletosCurso;
+import foro.dto.etiquetas.cursos.DatosGuardarCurso;
+import foro.servicios.EtiquetaServicio;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/cursos")
 public class NuevoCursoControlador {
 
+	@Autowired
+	private EtiquetaServicio etiquetaServicio;
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<DatosCompletosCurso> crearCurso(
+			@RequestBody
+			@Valid
+			DatosGuardarCurso datosCurso, 
+
+			UriComponentsBuilder uriComponentsBuilder) {
+
+		DatosCompletosCurso curso = etiquetaServicio.crearCurso(datosCurso);
+
+		URI url = uriComponentsBuilder.path("cursos/{cursoId}").buildAndExpand(curso.id()).toUri();
+
+		return ResponseEntity.created(url).body(curso);
+	}
 }
