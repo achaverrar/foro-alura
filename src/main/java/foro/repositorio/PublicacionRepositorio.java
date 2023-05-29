f45a8908b86c45d25230867b0f1cd27cc8e0e213
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 16:49:01 -0500

    Crea un método en PublicacionRepositorio para obtener todas las publicaciones, dado un curso

--- src/main/java/foro/repositorio/PublicacionRepositorio.java
+++ src/main/java/foro/repositorio/PublicacionRepositorio.java
@@ -1,8 +1,12 @@
 package foro.repositorio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import foro.modelo.Publicacion;
 
 public interface PublicacionRepositorio extends JpaRepository<Publicacion, Long> {
+
+	Page<Publicacion> findAllByCursoId(Long cursoId, Pageable paginacion);
 }
