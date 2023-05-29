fbef26f9d0e8476b2a9db146189898820be8d6a1
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 04:44:19 -0500

    Atiende peticiones de eliminar categorías desde el respectivo controlador

--- src/main/java/foro/controlador/CategoriaControlador.java
+++ src/main/java/foro/controlador/CategoriaControlador.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,5 +72,12 @@ public class CategoriaControlador {
 		DatosResumidosCategoria categoria = etiquetaServicio.editarCategoria(categoriaId, datosCategoria);
 
 		return ResponseEntity.ok(categoria);
+	}
+
+	@DeleteMapping("/{categoriaId}")
+	@Transactional
+	public ResponseEntity<Void> eliminarCategoria(@PathVariable Long categoriaId) {
+		etiquetaServicio.eliminarCategoria(categoriaId);
+		return ResponseEntity.noContent().build();
 	}
 }
