0b9851b0af995d6aca7956e51d6c5070a3fd7d80
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 04:36:40 -0500

    Atiende peticiones de eliminar subcategorías desde el controlador de subcategorías

--- src/main/java/foro/controlador/SubcategoriaControlador.java
+++ src/main/java/foro/controlador/SubcategoriaControlador.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,4 +88,12 @@ public class SubcategoriaControlador {
 
 		return ResponseEntity.ok(subcategoria);
 	}
+
+	@DeleteMapping("/{subcategoriaId}")
+	@Transactional
+	public ResponseEntity<Void> eliminarSubcategoria(@PathVariable Long subcategoriaId) {
+		etiquetaServicio.eliminarSubcategoria(subcategoriaId);
+		return ResponseEntity.noContent().build();
+	}
+
 }
