143ffcd15df69aacd9fd065d74554e0ad63df459
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 04:23:22 -0500

    Incluye los cursos en el DTO de DatosCompletosSubcategoria

--- src/main/java/foro/dto/etiquetas/subcategorias/DatosCompletosSubcategoria.java
+++ src/main/java/foro/dto/etiquetas/subcategorias/DatosCompletosSubcategoria.java
@@ -1,19 +1,24 @@
 package foro.dto.etiquetas.subcategorias;
 
+import java.util.List;
+
 import foro.dto.etiquetas.categorias.DatosResumidosCategoria;
+import foro.dto.etiquetas.cursos.DatosResumidosCurso;
 import foro.modelo.Etiqueta;
 
 public record DatosCompletosSubcategoria (
 		Long id,
 		String nombre,
-		DatosResumidosCategoria categoria
+		DatosResumidosCategoria categoria,
+		List<DatosResumidosCurso> cursos
 		) {
 
 	public DatosCompletosSubcategoria(Etiqueta subcategoria) {
 		this(
 				subcategoria.getId(),
 				subcategoria.getNombre(),
-				new DatosResumidosCategoria(subcategoria.getEtiquetaPadre())
+				new DatosResumidosCategoria(subcategoria.getEtiquetaPadre()),
+				subcategoria.getEtiquetasHijas().stream().map(DatosResumidosCurso::new).toList()
 			);
 	}
 }
