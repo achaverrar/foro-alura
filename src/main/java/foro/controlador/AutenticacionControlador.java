edfb1f30eee62123df789a8326626371bbf5ff5b
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 18:21:32 -0500

    Extrae la lógica de autenticación de UsuarioControlador en un ccontrolador independiente, exclusivo para la autenticación

---
+++ src/main/java/foro/controlador/AutenticacionControlador.java
@@ -0,0 +1,74 @@
+package foro.controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import foro.dto.usuarios.DatosIngresoUsuario;
+import foro.dto.usuarios.DatosRegistroUsuario;
+import foro.seguridad.DatosCompletosToken;
+import foro.seguridad.DatosTokensIngreso;
+import foro.seguridad.TokenServicio;
+import foro.servicios.UsuarioServicio;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1")
+public class AutenticacionControlador {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UsuarioServicio usuarioServicio;
+
+	@Autowired
+	private TokenServicio tokenServicio;
+
+	@PostMapping("/auth/login")
+	@Transactional
+	public ResponseEntity<Object> iniciarSesion(@RequestBody @Valid DatosIngresoUsuario datosUsuario) {
+		Authentication autenticacionToken = new UsernamePasswordAuthenticationToken(datosUsuario.correo(), datosUsuario.contrasena());
+		Authentication usuarioAutenticado = authenticationManager.authenticate(autenticacionToken);
+
+		DatosTokensIngreso tokens = tokenServicio.generarTokensDeIngreso(usuarioAutenticado);
+
+		return ResponseEntity.ok().body(tokens);
+	}
+
+	@PostMapping("/auth/signup")
+	@Transactional
+	public ResponseEntity<Object> registrarUsuario(@RequestBody @Valid DatosRegistroUsuario datosUsuario) {
+		usuarioServicio.registrarUsuario(datosUsuario);
+		return ResponseEntity.ok().body("Registro exitoso");
+	}
+
+	@PostMapping("auth/logout")
+	@Transactional
+	public ResponseEntity<Object> cerrarSesion() {
+		tokenServicio.eliminarRefreshTokenDeBD();
+		SecurityContextHolder.clearContext();
+		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping("auth/token/refresh")
+	public ResponseEntity<Object> refreshToken(
+			HttpServletRequest peticion, 
+			HttpServletResponse respuesta)
+			throws IOException {
+
+		DatosCompletosToken accessToken = tokenServicio.generarNuevoAccessToken(peticion);
+		return ResponseEntity.ok().body(accessToken);
+	}
+}
--- src/main/java/foro/controlador/UsuarioControlador.java
+++ src/main/java/foro/controlador/UsuarioControlador.java
@@ -4,10 +4,6 @@ import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,50 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import foro.dto.usuarios.DatosCambioContrasena;
-import foro.dto.usuarios.DatosIngresoUsuario;
-import foro.dto.usuarios.DatosRegistroUsuario;
-import foro.seguridad.DatosCompletosToken;
-import foro.seguridad.DatosTokensIngreso;
-import foro.seguridad.TokenServicio;
 import foro.servicios.UsuarioServicio;
-import io.jsonwebtoken.io.IOException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/usuarios")
 public class UsuarioControlador {
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
 
 	@Autowired
 	private UsuarioServicio usuarioServicio;
 
-	@Autowired
-	private TokenServicio tokenServicio;
-
-	@PostMapping("/auth/login")
-	@Transactional
-	public ResponseEntity<Object> iniciarSesion(@RequestBody @Valid DatosIngresoUsuario datosUsuario) {
-		Authentication autenticacionToken = new UsernamePasswordAuthenticationToken(datosUsuario.correo(), datosUsuario.contrasena());
-		Authentication usuarioAutenticado = authenticationManager.authenticate(autenticacionToken);
-
-		DatosTokensIngreso tokens = tokenServicio.generarTokensDeIngreso(usuarioAutenticado);
-
-		return ResponseEntity.ok().body(tokens);
-	}
-
-	@PostMapping("/auth/signup")
-	@Transactional
-	public ResponseEntity<Object> registrarUsuario(@RequestBody @Valid DatosRegistroUsuario datosUsuario) {
-		usuarioServicio.registrarUsuario(datosUsuario);
-		return ResponseEntity.ok().body("Registro exitoso");
-	}
-
-	@PostMapping("usuarios/{usuarioId}/roles/{rolId}")
+	@PostMapping("/{usuarioId}/roles/{rolId}")
 	@Transactional
 	public ResponseEntity<Object> asignarRolAUsuario(
 			@PathVariable Long usuarioId,
@@ -68,7 +32,7 @@ public class UsuarioControlador {
 		return ResponseEntity.ok().build();
 	}
 
-	@PutMapping("usuarios/contrasena")
+	@PutMapping("/contrasena")
 	@Transactional
 	public ResponseEntity<Object> cambiarContrasena(
 			Principal principal,
@@ -77,21 +41,4 @@ public class UsuarioControlador {
 		return ResponseEntity.ok().body("Contraseña cambiada con éxito");
 	}
 
-	@PostMapping("auth/logout")
-	@Transactional
-	public ResponseEntity<Object> cerrarSesion() {
-		tokenServicio.eliminarRefreshTokenDeBD();
-		SecurityContextHolder.clearContext();
-		return ResponseEntity.ok().build();
-	}
-
-	@PostMapping("auth/token/refresh")
-	public ResponseEntity<Object> refreshToken(
-			HttpServletRequest peticion, 
-			HttpServletResponse respuesta)
-			throws IOException {
-
-		DatosCompletosToken accessToken = tokenServicio.generarNuevoAccessToken(peticion);
-		return ResponseEntity.ok().body(accessToken);
-	}
 }
