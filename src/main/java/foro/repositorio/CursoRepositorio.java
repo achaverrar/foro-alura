ad934749ecfe915636e91e871737c268cb029cf5
Ayxa Chaverra <achaverrar@gmail.com>
2023-05-29 15:46:46 -0500

    Elimina los archivos de la anterior implementación de la entidad Curso

--- src/main/java/foro/repositorio/CursoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package foro.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import foro.modelo.Curso;
-
-public interface CursoRepositorio extends JpaRepository<Curso, Long> {
-}
